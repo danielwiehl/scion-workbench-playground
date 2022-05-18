8fd9161f8f967a2e0d266cbf0424809e31b56f80
Daniel Wiehl <daniel.wiehl@sbb.ch>
2022-05-18 12:18:57 +0200

    add desktop page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {WorkbenchModule} from '@scion/workbench';
+import {DesktopComponent} from './desktop/desktop.component';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DesktopComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    DesktopComponent,
   ],
   imports: [
     BrowserModule,
