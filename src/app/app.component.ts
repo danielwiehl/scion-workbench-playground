fba6933cebfec9669741a1b69b948d95693c4a01
Daniel Wiehl <daniel.wiehl@sbb.ch>
2022-05-18 12:20:13 +0200

    allow opening the desktop in a new tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {WorkbenchService} from '@scion/workbench';
+import {map, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  public viewCount$: Observable<number>;
+
+  constructor(workbench: WorkbenchService) {
+    this.viewCount$ = workbench.views$.pipe(map(views => views.length));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {WorkbenchModule} from '@scion/workbench';
+import {WB_VIEW_TITLE_PARAM, WorkbenchModule} from '@scion/workbench';
 import {DesktopComponent} from './desktop/desktop.component';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   {path: '', component: DesktopComponent},
+  {path: 'desktop', component: DesktopComponent, data: {[WB_VIEW_TITLE_PARAM]: 'New Tab'}},
 ];
 
 @NgModule({
