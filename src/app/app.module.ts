04fed1894f2c94e21f06d2bb706421b437db7bf4
Daniel Wiehl <daniel.wiehl@sbb.ch>
2022-05-18 12:16:08 +0200

    install @scion/workbench

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {WorkbenchModule} from '@scion/workbench';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [];
@@ -12,6 +14,8 @@ const routes: Routes = [];
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes, {useHash: true}),
+    BrowserAnimationsModule,
+    WorkbenchModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
